d633c6d162e097e333e211bd9b81ee79e5471699
Terrence Edmonds <terrenceedmonds31@gmail.com>
2019-03-05 23:16:32 -0500

    speed of sound added in. We tried many different methods for reducing the mass. We began running into an issue where the pressure did increase further across each node (which we wanted), but then began to explode. Currently we have an issue where towards the end of the running time for 1000 time steps, we get infinties or NaN entries in the energy change, which propagates problems.

--- fluid_cell.py
+++ fluid_cell.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cell_class as cc
 import matplotlib.pyplot as plt
+import sys
 
 
 def explicit_simulate_wind():
@@ -13,7 +14,7 @@ def explicit_simulate_wind():
     T_corona = 10**6 # temperature in Kelvin at the closest cell
 
     G = 6.674*(10**-11) # N m**2/kg**2
-    mass_sun = 1.989*(10**28) # kg
+    mass_sun = 1.989*(10**29) # kg
 
     radius_sun = 6.968*(10**8) # radius in meters
 
@@ -67,11 +68,15 @@ def explicit_simulate_wind():
     cell_density  = np.zeros(num_cells)
     cell_position = np.zeros(num_cells)
     cell_particles = np.zeros(num_cells)
+    cell_mass = np.zeros(num_cells)
+    cell_speed_of_sound = np.zeros(num_cells)
+    cell_massdensity = np.zeros(num_cells)
 
     node_area     = np.zeros(num_cells+1) # want a closing node
     node_velocity = np.zeros(num_cells+1) # these will stay zero until after the first time step
     node_position = np.zeros(num_cells+1) # should probably be called position
     node_gravity = np.zeros(num_cells+1)
+    node_mass = np.zeros(num_cells+1)
 
     """matrices for debugging"""
     cell_pressure_matrix = np.zeros(shape=[num_time_steps+1,num_cells])
@@ -79,11 +84,15 @@ def explicit_simulate_wind():
     cell_volume_matrix = np.zeros(shape=[num_time_steps+1,num_cells])
     cell_density_matrix = np.zeros(shape=[num_time_steps+1,num_cells])
     cell_position_matrix = np.zeros(shape=[num_time_steps+1,num_cells])
+    cell_mass_matrix = np.zeros(shape=[num_time_steps+1,num_cells])
+    cell_speed_of_sound_matrix = np.zeros(shape=[num_time_steps+1,num_cells])
+    cell_massdensity_matrix = np.zeros(shape=[num_time_steps+1,num_cells])
 
     node_velocity_matrix = np.zeros(shape=[num_time_steps+1,num_cells+1])
     node_position_matrix = np.zeros(shape=[num_time_steps+1,num_cells+1])
     node_area_matrix = np.zeros(shape=[num_time_steps+1,num_cells+1])
     node_gravity_matrix = np.zeros(shape=[num_time_steps+1,num_cells+1])
+    node_mass_matrix = np.zeros(shape=[num_time_steps+1,num_cells+1])
 
 
     node_position[0] = corona_inner
@@ -102,16 +111,34 @@ def explicit_simulate_wind():
 
         node_velocity[i+1] = 10**5 # 100 km/s
 
-        cell_particles[i] = mass_of_cell / mass_of_hydrogen_kg
-
-        cell_energy[i] = (3 / 2) * cell_particles[i]*kb_J * T_corona
         cell_volume[i] = (4/3)*np.pi*(node_position[i+1]**3
                                       - node_position[i]**3)
-        cell_density[i] = mass_of_cell/cell_volume[i]
+
+        cell_position[i] = (node_position[i] + node_position[i + 1]) / 2
+
+        #cell_mass[i] = mass_of_cell*np.exp(-cell_position[i]/cell_position[0])
+        cell_mass[i] = mass_of_cell * (cell_position[0] / cell_position[i])**2
+
+        if i < num_cells-1 :
+            node_mass[i+1] = (cell_mass[i]+cell_mass[i+1])/2
+        #cell_density[i] = mass_of_cell/cell_volume[i]
+        cell_density[i] = cell_mass[i]/cell_volume[i]
+        cell_massdensity[i] = cell_mass[i]/cell_volume[i]
+
+        cell_particles[i] = cell_mass[i] / mass_of_hydrogen_kg
+
+
+        cell_energy[i] = (3 / 2) * cell_particles[i]*kb_J * T_corona
+
+
+
+
         #cell_density[i] = 1 / cell_volume[i]
         #cell_pressure[i] = (gamma-1)*cell_density[i]*cell_energy[i]
-        cell_position[i] = (node_position[i] + node_position[i+1])/2
+
         cell_pressure[i] = (num_moles*R*T_corona)/cell_volume[i]
+
+        cell_speed_of_sound[i] = np.sqrt(gamma*(cell_pressure[i]/cell_massdensity[i]))
 
         node_position_matrix[0,i+1] = node_position[i+1]
         node_area_matrix[0,i+1] = node_area[i+1]
@@ -121,27 +148,32 @@ def explicit_simulate_wind():
         cell_density_matrix[0,i] = cell_density[i]
         node_gravity_matrix[0,i+1] = node_gravity[i+1]
         node_velocity_matrix[0,i+1] = node_velocity[i+1]
+        cell_speed_of_sound_matrix[0,i] = cell_speed_of_sound[i]
+        cell_position_matrix[0,i] = cell_position[i]
 
 
+
+    node_mass[0] = cell_mass[0]/2
+    node_mass[num_cells] = cell_mass[num_cells-1]/2
 
     """initialization complete"""
 
 
     """make some initial plots to help verify how the system evolves"""
-    plt.plot(cell_position/radius_sun,cell_pressure,'go')
-    plt.title('initial pressure vs position')
-    plt.legend()
-    plt.show()
+    #plt.plot(cell_position/radius_sun,cell_pressure,'go')
+    #plt.title('initial pressure vs position')
+    #plt.legend()
+    #plt.show()
 
-    plt.plot(cell_position / radius_sun, cell_energy, 'go')
-    plt.title('initial energy vs position')
-    plt.legend()
-    plt.show()
+    #plt.plot(cell_position / radius_sun, cell_energy, 'go')
+    #plt.title('initial energy vs position')
+    #plt.legend()
+    #plt.show()
 
-    plt.plot(node_position/radius_sun,node_velocity,'go')
-    plt.title('initial velocity vs position')
-    plt.legend()
-    plt.show()
+    #plt.plot(node_position/radius_sun,node_velocity,'go')
+    #plt.title('initial velocity vs position')
+    #plt.legend()
+    #plt.show()
 
 
     """now onto updating during timesteps"""
@@ -158,7 +190,7 @@ def explicit_simulate_wind():
             node_position[j + 1] = node_position[j + 1] + node_velocity_matrix[i, j + 1] * time_step_length
 
             node_velocity[j+1] = evolve_velocity(velocity=node_velocity,pressure=cell_pressure,
-                                                 d_time=time_step_length,area=node_area,mass=mass_of_cell,
+                                                 d_time=time_step_length,area=node_area,mass=node_mass,
                                                  gravity=node_gravity,j=j,limit=num_cells)
 
             node_area[j+1] = 4*np.pi*(node_position[j+1]**2)
@@ -168,8 +200,12 @@ def explicit_simulate_wind():
             cell_volume[j] = (4/3)*np.pi*(node_position[j+1]**3 - node_position[j]**3)
             """experimenting """
 
+            cell_massdensity[j] = cell_mass[j] / cell_volume[j]
+
             cell_density[j] = 1 / cell_volume[j]
             cell_pressure[j] = (gamma-1)*cell_density[j]*cell_energy[j]
+
+            cell_position[j] = (node_position[j] + node_position[j + 1]) / 2
 
             #cell_density[j] = mass_of_cell / cell_volume[j]
             #cell_pressure[j] = (num_moles * R * T_corona) / cell_volume[j]
@@ -181,24 +217,33 @@ def explicit_simulate_wind():
             cell_volume_matrix[i+1,j] = cell_volume[j]
             cell_density_matrix[i+1,j] = cell_density[j]
             cell_pressure_matrix[i+1,j] = cell_pressure[j]
-
+            cell_position_matrix[i+1,j] = cell_position[j]
+            cell_speed_of_sound_matrix[i+1,j] = np.sqrt(gamma*(cell_pressure[j]/cell_massdensity[j]))
 
             #if(node_velocity[j+1] < 0):
                 #print("negative velocity! likely error",j,i)
                 #return
 
-        if i%(100) == 0 :
-            plt.plot(node_position / radius_sun, node_velocity/1000,'bo')
-            plt.show()
+        #if i%(100) == 0 :
+            #plt.plot(node_position / radius_sun, node_velocity/1000,'bo')
+            #plt.show()
 
     #print(node_position[50],node_velocity[50])
 
-    print('\npressure \n',cell_pressure_matrix[0:800:100,:])
-    print('\nvelocity \n',node_velocity_matrix[0:800:100,:]/1000)
+    plt.plot(node_position_matrix[900,:] / radius_sun, node_velocity_matrix[900,:]/1000,'bo')
+    plt.plot(cell_position_matrix[900,:]/radius_sun,cell_speed_of_sound_matrix[900,:]/1000,'go')
+    plt.show()
+
+    np.set_printoptions(threshold=np.inf)
+
+    #print('\npressure \n',cell_pressure_matrix[0:1000:100,:])
+    #print('\npressure \n', cell_pressure_matrix[0, :])
+    #print('\nmass\n',cell_particles)
+    print('\nvelocity \n',node_velocity_matrix[num_time_steps-15,:]/1000)
     #print('\nposition \n',node_position_matrix[:,:]/radius_sun)
-    print('\nenergy\n',cell_energy_matrix[0:800:100,:])
-    print('\nvolume\n',cell_volume_matrix[0,:])
-    print('\ndensity\n',cell_density_matrix[0,:])
+    print('\nenergy\n',cell_energy_matrix[num_time_steps-15,:])
+    #print('\nvolume\n',cell_volume_matrix[0,:])
+    #print('\ndensity\n',cell_density_matrix[0,:])
     #print('\narea\n',node_area_matrix[0,:])
     #print('\ngravity\n',node_gravity_matrix[0,:])
 
@@ -208,6 +253,8 @@ def explicit_simulate_wind():
     total_energy_start = sum(node_velocity_matrix[0, :] ** 2) + sum(cell_energy_matrix[0, :])
     total_energy_end = sum(node_velocity_matrix[num_time_steps - 1, :] ** 2) + sum(
         cell_energy_matrix[num_time_steps - 1, :])
+    print(total_energy_start)
+    print(total_energy_end)
     print("End Total energy is ", total_energy_end / total_energy_start, " from start energy")
 
     return
@@ -274,7 +321,9 @@ def evolve_energy(velocity,pressure,area,d_time,count,limit,energy_matrix,time_s
         #print(p_area)
         #print('\ndelta energy = ',d_energy,'\n')
 
-
+    if d_energy == np.inf or d_energy == np.nan :
+        print("overflow energy at ",time_step)
+        #sys.exit()
 
     return d_energy
 
@@ -286,7 +335,7 @@ def evolve_velocity(velocity,pressure,d_time,area,mass,gravity,j,limit):
     if(j<cell_limit):
         p_diff = pressure[j] - pressure[j+1]
         grav_t = gravity[j+1]*d_time
-        p_vel = p_diff*area[j+1]*d_time/mass
+        p_vel = p_diff*area[j+1]*d_time/mass[j+1]
 
         new_vel = velocity[j+1] + p_vel + grav_t
 
@@ -297,12 +346,16 @@ def evolve_velocity(velocity,pressure,d_time,area,mass,gravity,j,limit):
     if(j==cell_limit):
         p_diff= pressure[j] - pressure[j]/2
         grav_t = gravity[j+1]*d_time
-        p_vel = p_diff*area[j+1]*d_time/mass
+        p_vel = p_diff*area[j+1]*d_time/mass[j+1]
 
         new_vel = velocity[j+1] + p_vel + grav_t
 
         #if (pressure[j]) < 10**-5*pressure_matrix[0,j] :
         #    new_vel = velocity[j+1] + grav_t
+
+    if new_vel == np.inf or new_vel == np.nan :
+        print("overflow velocity at ",j)
+        #sys.exit()
 
 
     return new_vel
